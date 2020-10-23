e02f971302f145701fccbb39fad16212958df256
adam allalou <aabou9@gmail.com>
2020-10-23 15:32:16 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ErrorExample from '../src/tutorial/1-useState/setup/1-error-example';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <ErrorExample/>
     </div>
   );
 }
 
 export default App;
+ 
