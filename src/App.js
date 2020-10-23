c902e164de4e711c74e01e48a0ccf1a8c93b9ff2
adam allalou <aabou9@gmail.com>
2020-10-23 19:59:41 +0100

    add useState-array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Setup from '../src/tutorial/1-useState/setup/2-useState-basics';
-import Final from '../src/tutorial/1-useState/final/2-useState-basics';
+import Setup from '../src/tutorial/1-useState/setup/3-useState-array';
+import Final from '../src/tutorial/1-useState/final/3-useState-array';
 function App() {
   return (
     <div className='container'>
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,29 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const clickHandler = () => {
+    setPeopol([])
+    console.log('this is a click hander of clear all')
+  }
+  const deleteItem = (id) => {
+    console.log('this is a delete Items')
+
+    const newPeopol = peopol.filter((person) => {
+      return person.id !== id;
+    })
+    setPeopol(newPeopol)
+  }
+  const [peopol, setPeopol] = React.useState(data)
+  return <>
+  {peopol.map((person)=>{
+    const {id, name} = person
+    return <div className='item'>
+      <h4  key={id}>{name}</h4>
+      <button onClick={() => deleteItem(id)}>remove</button>
+    </div>
+  })}
+  <button className='btn' onClick={clickHandler}>clear items</button>
+  </>
 };
 
 export default UseStateArray;
