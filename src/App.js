8d0c766f1a465bb49e86c53e389d2fc10529e74d
adam allalou <aabou9@gmail.com>
2020-10-24 16:30:18 +0100

    useState-object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Setup from '../src/tutorial/1-useState/setup/3-useState-array';
-import Final from '../src/tutorial/1-useState/final/3-useState-array';
+import Setup from '../src/tutorial/1-useState/setup/4-useState-object';
+import Final from '../src/tutorial/1-useState/final/4-useState-object';
 function App() {
   return (
     <div className='container'>
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+  const clickHandler = () => {
+    setPerson({...person , message: 'hello world'})
+  }
+  let {name, age, message} = person;
+  return <>
+  <h4>{name}</h4>
+  <h4>{age}</h4>
+  <h4>{message}</h4>
+  <button className='btn' onClick={clickHandler}>change</button>
+  </>;
 };
 
 export default UseStateObject;
