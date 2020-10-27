ef4053ee6ef44ed297df88e6385d19550a6606b0
adam allalou <aabou9@gmail.com>
2020-10-27 15:07:19 +0100

    add controlled-input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Setup from '../src/tutorial/3-conditional-rendering/setup/2-short-circuit';
-import Final from '../src/tutorial/3-conditional-rendering/final/2-short-circuit';
+import Setup from '../src/tutorial/4-forms/setup/1-controlled-inputs';
+import Final from '../src/tutorial/4-forms/final/1-controlled-inputs';
 
 function App() {
   return (
--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -4,9 +4,54 @@ import React, { useState } from 'react';
 // const inputValue = input.value
 // React
 // value, onChange
-
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [data, setData] = useState('')
+  const [people, setPeople] = useState([])
+const handleSubmit = (e) => {
+  e.preventDefault()
+  // setData(e.target.value)
+
+  // people.push(document.getElementById('data').value)
+  setPeople((people) => {
+    return [...people, document.getElementById('data').value]
+  })
+  setData(document.getElementById('data').value)
+  // console.log(document.getElementById('data').value);
+}
+
+
+const addperson = (person) => {
+}
+
+
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='data'>Name : </label>
+            <input
+              type='text'
+              id='data'
+              name='data'
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+            />
+          </div>
+          <button onClick={handleSubmit} type='submit'>add data</button>
+          <h1>{data}</h1>
+          {people.map((person, index) => {
+          return (
+            <div className='item' key={index}>
+              <h4>{person}</h4>
+            </div>
+          );
+        })}
+        {/* {console.log(people)} */}
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
